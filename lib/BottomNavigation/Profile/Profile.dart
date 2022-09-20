8f6723d13cd47464e24d892e181d78ca6b2f25a7
Mohamed Ibrahem <mh18399@gmail.com>
2022-09-20 02:32:14 +0200

    refined user profile ui

--- lib/BottomNavigation/Profile/Profile.dart
+++ lib/BottomNavigation/Profile/Profile.dart
@@ -17,119 +17,173 @@ class _ProfileScreenState extends State<ProfileScreen> {
   late Map<String, dynamic> data;
 
   @override
+  void initState() {
+    _fetch();
+    super.initState();
+  }
+
+  var formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Profile"),
         automaticallyImplyLeading: false,
         centerTitle: true,
-        // actions: [
-        //   IconButton(
-        //       onPressed: () async {
-        //         final prefs = await SharedPreferences.getInstance();
-        //         prefs.setBool('showHome', false);
-        //
-        //         Navigator.of(context).pushReplacement(
-        //             MaterialPageRoute(builder: (context) => OnBording()));
-        //       },
-        //       icon: const Icon(Icons.logout))
-        // ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FutureBuilder(
-              future: _fetch(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState != ConnectionState.done)
-                  return Text("Loading data...Please wait");
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                            width: double.infinity,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.black12,
+      body: SafeArea(
+        child: Center(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: FutureBuilder(
+                    future: _fetch(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState != ConnectionState.done)
+                        return Text("Loading data...Please wait");
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            readOnly: true,
+                            keyboardType: TextInputType.emailAddress,
+                            initialValue: data['email'],
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "email address must not be empty";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Email",
+                              prefixIcon: Icon(Icons.email),
+                              border: OutlineInputBorder(),
                             ),
-                            child: Padding(
-                              padding: EdgeInsets.only(top: 17, left: 10),
-                              child: Text(
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold),
-                                  "Name: ${data['first name']} ${data['last name']}"),
-                            )),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          width: double.infinity,
-                          height: 70,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.black12,
                           ),
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 17, left: 10),
-                            child: Text(
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
+                          SizedBox(
+                            height: 25,
+                          ),
+                          TextFormField(
+                            readOnly: true,
+                            keyboardType: TextInputType.text,
+                            initialValue:
+                                "${data['first name']} ${data['last name']}",
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return "first name must not be empty";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              labelText: "Name",
+                              prefixIcon: Icon(Icons.account_circle),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.datetime,
+                            initialValue: DateFormat('yyyy-MM-dd')
+                                .format(data['birth date'].toDate()),
+                            decoration: InputDecoration(
+                              labelText: "Birth date",
+                              prefixIcon: Icon(Icons.calendar_today),
+                              border: OutlineInputBorder(),
+                            ),
+                            readOnly: true,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                "Birth date: ${DateFormat('yyyy-MM-dd').format(data['birth date'].toDate())}"),
+                                width: 150,
+                                child: MaterialButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => YourPosts(),
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    "My Posts",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                width: 150,
+                                child: MaterialButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => EditProfile(),
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    "Edit",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Center(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              width: 150,
+                              child: MaterialButton(
+                                onPressed: () {
+                                  FirebaseAuth.instance.signOut();
+                                },
+                                child: Text(
+                                  "Sign out",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }),
+              ),
             ),
-            Text(
-                style: TextStyle(fontSize: 20), 'signed in as: ' + user.email!),
-            MaterialButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              color: Colors.grey,
-              child: Text('sign out'),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => YourPosts(),
-                  ),
-                );
-              },
-              child: Text("My Posts"),
-              color: Colors.grey,
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EditProfile(),
-                  ),
-                );
-              },
-              child: Text("Edit"),
-              color: Colors.grey,
-            )
-          ],
+          ),
         ),
       ),
     );
